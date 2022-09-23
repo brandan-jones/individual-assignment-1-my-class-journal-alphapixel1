3747d3b72dd5c01c7898b2d0fba96e0515256f12
alphapixel1 <alphapixel2@gmail.com>
2022-09-23 03:39:51 -0400

    added javadocs

--- src/main/java/edu/uc/cech/soit/myclassjournal/service/Journal.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/service/Journal.java
@@ -9,11 +9,20 @@ import java.util.List;
 @Service
 public class Journal implements IJournalService{
     private List<JournalEntry> journalEntries=new ArrayList<>();
+
+    /**
+     * Saves the entry to the journal
+     * @param journalEntry the entry to save.
+     */
     @Override
     public void save(JournalEntry journalEntry) {
         journalEntries.add(journalEntry);
     }
 
+    /**
+     * Fetches all Journal entries
+     * @return a list of all journal entries
+     */
     @Override
     public List<JournalEntry> fetchAll() {
         return journalEntries;
